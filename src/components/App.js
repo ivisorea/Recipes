f09fc9abfdd4c01425edf001c36a5f40dc70d3a1
Ivis Orea <ivisorea@gmail.com>
2021-11-02 21:43:10 +0100

    make changes in index.html

--- src/components/App.js
+++ src/components/App.js
@@ -7,11 +7,7 @@ import './App.css';
 const App = () => {
   return (
     <>
-     <Row className="mx-0">
-      <Button as={Col} variant="primary">Button #1</Button>
-      <Button as={Col} variant="secondary" className="mx-2">Button #2</Button>
-      <Button as={Col} variant="success">Button #3</Button>
-    </Row>
+     
     </>
   )
 }
