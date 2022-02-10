6094ab1dfae873f9f6c6332e0fb902d50e8fd4c9
Ivis Orea <ivisorea@gmail.com>
2022-02-10 13:10:53 +0100

    [EDIT] Solving some issues

--- src/pages/ContactUs/index.js
+++ src/pages/ContactUs/index.js
@@ -1,7 +1,6 @@
 import React, { useRef } from "react";
 import emailjs from '@emailjs/browser';
 import './styles.css';
-import { Row } from "react-bootstrap";
 
 export const ContactUs = () => {
     const form = useRef();
